4156c509012a1cb9be96bf573934182fb2f4b6a2
SupremeTurnerTrueCoders <email@example.com>
2024-04-16 00:02:09 -0400

    fully completed exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.ComponentModel.Design;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            var answer =  4; 
+            var response = (answer < 9) ? $"{answer} is less than Nine" : $"{answer} Greater than or equal to Nine";
+            Console.WriteLine(response);
+        }  
+       
     }
 }
+
+
+
+                  
